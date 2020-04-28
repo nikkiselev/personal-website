675ea44f9fb5da41591d3ba824b381d2c9e475d3
Nikolai Kiselev <nik@kiselev.dev>
2020-04-28 22:30:52 +0400

    Add spacing to divider

--- src/js/components/PageDivider.tsx
+++ src/js/components/PageDivider.tsx
@@ -1,6 +1,17 @@
-import { Divider } from '@material-ui/core'
+import { Divider, Theme, makeStyles, createStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      marginTop: theme.spacing(5),
+      marginBottom: theme.spacing(5),
+    },
+  })
+)
 
 const PageDivider = () => {
-  return <Divider />
+  const classes = useStyles()
+
+  return <Divider className={classes.root} />
 }
 export default PageDivider
