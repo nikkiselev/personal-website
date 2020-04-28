2d2e27aad0bff6ce3dd87b4778fa9d676b75adc0
Nikolai Kiselev <nik@kiselev.dev>
2020-04-28 22:01:09 +0400

    Fix footer styles

--- src/js/components/footer.tsx
+++ src/js/components/footer.tsx
@@ -1,15 +1,29 @@
-import { Grid, createStyles, makeStyles, Typography } from '@material-ui/core'
-import styles from '../../css/index.module.scss'
+import {
+  Grid,
+  createStyles,
+  makeStyles,
+  Typography,
+  Link,
+  Theme,
+} from '@material-ui/core'
 import FooterBar from './FooterBar'
 import FooterMail from './FooterMail'
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     title: {
       fontWeight: 'bold',
+      marginBottom: theme.spacing(1),
     },
     content: {
       textAlign: 'center',
+    },
+    text: {
+      // marginBottom: theme.spacing(2),
+    },
+    item: {
+      marginBottom: theme.spacing(1),
+      marginTop: theme.spacing(1),
     },
   })
 )
@@ -20,35 +34,34 @@ const Footer = () => {
   return (
     <>
       <Grid container className={classes.content}>
-        <Grid item xs={12} className={styles.work}>
+        <Grid item xs={12} className={classes.item}>
           <Typography variant="h5" className={classes.title}>
             Work with me
           </Typography>
 
-          <Typography variant="body1">
+          <Typography variant="body1" className={classes.text}>
             I'm a practical person so if you have a cool project to work on or
             share the feeling that I could be a good fit with your team, reach
             out and let's have a chat!
           </Typography>
+        </Grid>
 
+        <Grid item xs={12} className={classes.item}>
           <FooterMail />
         </Grid>
 
-        <Grid item xs={12} className={styles.links}>
-          <p className={styles.profileText}>I'm also on</p>
+        <Grid item xs={12} className={classes.item}>
+          <Typography variant="body1">I'm also on</Typography>
+        </Grid>
 
-          <a
-            className={styles.profileLink}
-            href="https://github.com/nikkiselev"
-          >
-            GitHub
-          </a>
-          <a
-            className={styles.profileLink}
-            href="https://stackoverflow.com/users/5964023/nikolai-kiselev?tab=profile"
-          >
+        <Grid item xs={12} className={classes.item}>
+          <Link href="https://github.com/nikkiselev">GitHub</Link>
+        </Grid>
+
+        <Grid item xs={12} className={classes.item}>
+          <Link href="https://stackoverflow.com/users/5964023/nikolai-kiselev?tab=profile">
             Stack Overflow
-          </a>
+          </Link>
         </Grid>
       </Grid>
 
