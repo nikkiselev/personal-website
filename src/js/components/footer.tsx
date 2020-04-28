3b89a71738e606d8fb6ff462fb7708e2e1f10917
Nikolai Kiselev <nik@kiselev.dev>
2020-04-29 00:10:52 +0400

    Refactor

--- src/js/components/footer.tsx
+++ src/js/components/footer.tsx
@@ -1,68 +1,56 @@
-import {
-  Grid,
-  createStyles,
-  makeStyles,
-  Typography,
-  Link,
-  Theme,
-} from '@material-ui/core'
+import { Grid, Typography, Link, Box } from '@material-ui/core'
 import FooterBar from './FooterBar'
 import FooterMail from './FooterMail'
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    title: {
-      fontWeight: 'bold',
-      marginBottom: theme.spacing(1),
-    },
-    content: {
-      textAlign: 'center',
-    },
-    text: {
-      // marginBottom: theme.spacing(2),
-    },
-    item: {
-      marginBottom: theme.spacing(1),
-      marginTop: theme.spacing(1),
-    },
-  })
-)
+const itemSpacing = 1
 
 const Footer = () => {
-  const classes = useStyles()
-
   return (
     <>
-      <Grid container className={classes.content}>
-        <Grid item xs={12} className={classes.item}>
-          <Typography variant="h5" className={classes.title}>
-            Work with me
-          </Typography>
+      <Grid container>
+        <Box textAlign="center">
+          <Grid item xs={12}>
+            <Box my={itemSpacing}>
+              <Typography variant="h5">
+                <Box fontWeight="bold" mb={1}>
+                  Work with me
+                </Box>
+              </Typography>
 
-          <Typography variant="body1" className={classes.text}>
-            I'm a practical person so if you have a cool project to work on or
-            share the feeling that I could be a good fit with your team, reach
-            out and let's have a chat!
-          </Typography>
-        </Grid>
+              <Typography variant="body1">
+                I'm a practical person so if you have a cool project to work on
+                or share the feeling that I could be a good fit with your team,
+                reach out and let's have a chat!
+              </Typography>
+            </Box>
+          </Grid>
 
-        <Grid item xs={12} className={classes.item}>
-          <FooterMail />
-        </Grid>
+          <Grid item xs={12}>
+            <Box my={itemSpacing}>
+              <FooterMail />
+            </Box>
+          </Grid>
 
-        <Grid item xs={12} className={classes.item}>
-          <Typography variant="body1">I'm also on</Typography>
-        </Grid>
+          <Grid item xs={12}>
+            <Box my={itemSpacing}>
+              <Typography variant="body1">I'm also on</Typography>
+            </Box>
+          </Grid>
 
-        <Grid item xs={12} className={classes.item}>
-          <Link href="https://github.com/nikkiselev">GitHub</Link>
-        </Grid>
+          <Grid item xs={12}>
+            <Box my={itemSpacing}>
+              <Link href="https://github.com/nikkiselev">GitHub</Link>
+            </Box>
+          </Grid>
 
-        <Grid item xs={12} className={classes.item}>
-          <Link href="https://stackoverflow.com/users/5964023/nikolai-kiselev?tab=profile">
-            Stack Overflow
-          </Link>
-        </Grid>
+          <Grid item xs={12}>
+            <Box my={itemSpacing}>
+              <Link href="https://stackoverflow.com/users/5964023/nikolai-kiselev?tab=profile">
+                Stack Overflow
+              </Link>
+            </Box>
+          </Grid>
+        </Box>
       </Grid>
 
       <FooterBar />
