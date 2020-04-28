da0b40f74326c62b51769c325684e626fd76b220
Nikolai Kiselev <nik@kiselev.dev>
2020-04-28 20:21:46 +0400

    Install material UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,38 @@
+import React, { useEffect } from 'react'
+import Head from 'next/head'
 import { AppProps } from 'next/app'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from '../theme'
 import '../css/app.scss'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+export default ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.getElementById('jss-server-side')
+    if (jssStyles) {
+      ;(jssStyles.parentElement as HTMLElement).removeChild(jssStyles)
+    }
+  }, [])
 
-export default MyApp
+  return (
+    <>
+      <Head>
+        <title>Nikolai Kiselev - full stack web developer</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-135544092-1"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
