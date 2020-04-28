bcef9664f5ab1f364289abc6bf58e23ce01e1ba3
Nikolai Kiselev <nik@kiselev.dev>
2020-04-28 21:10:50 +0400

    Move work with me to footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -84,37 +84,6 @@ const Home = () => {
       </Grid>
 
       <Divider />
-
-      <Grid container>
-        <Grid item xs={12} className={styles.work}>
-          <h3>Work with me</h3>
-          <p>
-            I'm a practical person so if you have a cool project to work on or
-            share the feeling that I could be a good fit with your team, reach
-            out and let's have a chat!
-          </p>
-          <div className={styles.workEmail}>
-            <img src="/images/mail_icon.png" className={styles.mail} />
-            <a href="mailto:nik@kiselev.dev">nik@kiselev.dev</a>
-          </div>
-        </Grid>
-        <Grid item xs={12} className={styles.links}>
-          <p className={styles.profileText}>I'm also on</p>
-
-          <a
-            className={styles.profileLink}
-            href="https://github.com/nikkiselev"
-          >
-            GitHub
-          </a>
-          <a
-            className={styles.profileLink}
-            href="https://stackoverflow.com/users/5964023/nikolai-kiselev?tab=profile"
-          >
-            Stack Overflow
-          </a>
-        </Grid>
-      </Grid>
     </Container>
   )
 }
