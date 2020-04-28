f814e970f06563d4128927ae917e35bf5dda9912
Nikolai Kiselev <nik@kiselev.dev>
2020-04-29 00:22:00 +0400

    Refactor

--- src/js/components/FooterBar.tsx
+++ src/js/components/FooterBar.tsx
@@ -1,36 +1,26 @@
-import {
-  Grid,
-  Typography,
-  createStyles,
-  makeStyles,
-  Theme,
-} from '@material-ui/core'
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      marginTop: theme.spacing(8),
-      backgroundColor: '#1f2020',
-      color: 'white',
-      height: '100px',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  })
-)
+import { Grid, Typography, Box } from '@material-ui/core'
 
 const FooterBar = () => {
-  const classes = useStyles()
-
   return (
-    <Grid container>
-      <Grid item xs={12} className={classes.root}>
-        <Typography>
-          &copy; Nikolai Kiselev {new Date().getFullYear()}
-        </Typography>
+    <Box mt={8}>
+      <Grid container>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          height={100}
+          color="white"
+          bgcolor="#1f2020"
+          clone
+        >
+          <Grid item xs={12}>
+            <Typography component="div">
+              &copy; Nikolai Kiselev {new Date().getFullYear()}
+            </Typography>
+          </Grid>
+        </Box>
       </Grid>
-    </Grid>
+    </Box>
   )
 }
 
