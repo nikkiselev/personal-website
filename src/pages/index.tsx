2a0772ea2db80337d183c36199d12be263abeb1f
Nikolai Kiselev <nik@kiselev.dev>
2020-04-29 00:40:52 +0400

    Refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,11 @@
-import {
-  Container,
-  Grid,
-  Typography,
-  makeStyles,
-  createStyles,
-  Theme,
-} from '@material-ui/core'
+import { Container, Grid, Typography, Box } from '@material-ui/core'
 import PageDivider from '../js/components/PageDivider'
 import SubHeader from '../js/components/SubHeader'
 import Avatar from '../js/components/Avatar'
 
 import Location from '../js/components/Location'
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    intro: {
-      textAlign: 'center',
-      marginTop: theme.spacing(5),
-    },
-    lastText: {
-      marginBottom: theme.spacing(3),
-    },
-  })
-)
-
 const Home = () => {
-  const classes = useStyles()
-
   return (
     <>
       <SubHeader />
@@ -37,23 +16,27 @@ const Home = () => {
         <Grid container justify="center">
           <Avatar />
 
-          <Grid item xs={12} className={classes.intro}>
-            <Typography variant="body1">
-              I still remember writing small scripts and apps using Pascal when
-              I was 12 years old just for fun. Fast forward to today, I cannot
-              see myself doing anything else but web development.
-            </Typography>
+          <Box textAlign="center" mt={5}>
+            <Grid item xs={12}>
+              <Typography variant="body1">
+                I still remember writing small scripts and apps using Pascal
+                when I was 12 years old just for fun. Fast forward to today, I
+                cannot see myself doing anything else but web development.
+              </Typography>
 
-            <br />
+              <br />
 
-            <Typography variant="body1" className={classes.lastText}>
-              As a full stack developer, I can build seamless & scalable web
-              applications, websites. Whether it’s from scratch or an existing
-              codebase, I’m ready to jump in.
-            </Typography>
+              <Box mb={3}>
+                <Typography variant="body1">
+                  As a full stack developer, I can build seamless & scalable web
+                  applications, websites. Whether it’s from scratch or an
+                  existing codebase, I’m ready to jump in.
+                </Typography>
+              </Box>
 
-            <Location />
-          </Grid>
+              <Location />
+            </Grid>
+          </Box>
         </Grid>
       </Container>
 
