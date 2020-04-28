dc408c9e8358006097755af736f070608b6fcd42
Nikolai Kiselev <nik@kiselev.dev>
2020-04-29 00:44:18 +0400

    Wrap footer into container

--- src/js/components/footer.tsx
+++ src/js/components/footer.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography, Link, Box } from '@material-ui/core'
+import { Grid, Typography, Link, Box, Container } from '@material-ui/core'
 import FooterBar from './FooterBar'
 import FooterMail from './FooterMail'
 
@@ -7,52 +7,53 @@ const itemSpacing = 1
 const Footer = () => {
   return (
     <>
-      <Grid container>
-        <Box textAlign="center">
-          <Grid item xs={12}>
-            <Box my={itemSpacing}>
-              <Typography variant="h5">
-                <Box fontWeight="bold" mb={1}>
-                  Work with me
-                </Box>
-              </Typography>
+      <Container>
+        <Grid container>
+          <Box textAlign="center">
+            <Grid item xs={12}>
+              <Box my={itemSpacing}>
+                <Typography variant="h5">
+                  <Box fontWeight="bold" mb={1}>
+                    Work with me
+                  </Box>
+                </Typography>
 
-              <Typography variant="body1">
-                I'm a practical person so if you have a cool project to work on
-                or share the feeling that I could be a good fit with your team,
-                reach out and let's have a chat!
-              </Typography>
-            </Box>
-          </Grid>
+                <Typography variant="body1">
+                  I'm a practical person so if you have a cool project to work
+                  on or share the feeling that I could be a good fit with your
+                  team, reach out and let's have a chat!
+                </Typography>
+              </Box>
+            </Grid>
 
-          <Grid item xs={12}>
-            <Box my={itemSpacing}>
-              <FooterMail />
-            </Box>
-          </Grid>
+            <Grid item xs={12}>
+              <Box my={itemSpacing}>
+                <FooterMail />
+              </Box>
+            </Grid>
 
-          <Grid item xs={12}>
-            <Box my={itemSpacing}>
-              <Typography variant="body1">I'm also on</Typography>
-            </Box>
-          </Grid>
+            <Grid item xs={12}>
+              <Box my={itemSpacing}>
+                <Typography variant="body1">I'm also on</Typography>
+              </Box>
+            </Grid>
 
-          <Grid item xs={12}>
-            <Box my={itemSpacing}>
-              <Link href="https://github.com/nikkiselev">GitHub</Link>
-            </Box>
-          </Grid>
+            <Grid item xs={12}>
+              <Box my={itemSpacing}>
+                <Link href="https://github.com/nikkiselev">GitHub</Link>
+              </Box>
+            </Grid>
 
-          <Grid item xs={12}>
-            <Box my={itemSpacing}>
-              <Link href="https://stackoverflow.com/users/5964023/nikolai-kiselev?tab=profile">
-                Stack Overflow
-              </Link>
-            </Box>
-          </Grid>
-        </Box>
-      </Grid>
-
+            <Grid item xs={12}>
+              <Box my={itemSpacing}>
+                <Link href="https://stackoverflow.com/users/5964023/nikolai-kiselev?tab=profile">
+                  Stack Overflow
+                </Link>
+              </Box>
+            </Grid>
+          </Box>
+        </Grid>
+      </Container>
       <FooterBar />
     </>
   )
