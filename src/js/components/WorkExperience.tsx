1cf28e32658f19c180529c626c355575dc438a99
Nikolai Kiselev <nik@kiselev.dev>
2020-04-29 18:39:09 +0400

    Refactor

--- src/js/components/WorkExperience.tsx
+++ src/js/components/WorkExperience.tsx
@@ -10,25 +10,32 @@ import {
 } from '@material-ui/core'
 import PageSubTitle from './PageSubTitle'
 import jobs from '../../data/jobs'
+import { Job as JobType } from '../../types'
 
-const Job = (props: any) => (
+type Props = {
+  job: JobType
+}
+
+const Job = ({
+  job: { role, company, from, to, country, responsibilities, skills },
+}: Props) => (
   <>
-    <Typography variant="h5">{props.job.role}</Typography>
+    <Typography variant="h5">{role}</Typography>
     <Typography variant="subtitle1">
-      {props.job.company}, {props.job.from}-{props.job.to}
+      {company}, {from}-{to}
     </Typography>
     <Typography variant="subtitle1" color="textSecondary">
-      {props.job.country}
+      {country}
     </Typography>
 
     <List>
-      {props.job.responsibilities.map((item: string) => (
+      {responsibilities.map((item: string) => (
         <ListItem key={item}>
           <ListItemText>{item}.</ListItemText>
         </ListItem>
       ))}
     </List>
-    {props.job.skills.map((label: string) => (
+    {skills.map((label: string) => (
       <Box mr={1} key={label} clone>
         <Chip label={label} color="primary" />
       </Box>
