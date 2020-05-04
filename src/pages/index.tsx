05b6a4fad49b9e7c3ba5c2fcd627f42123866fef
Nikolai Kiselev <nik@kiselev.dev>
2020-05-04 17:44:43 +0400

    Add Education section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,8 @@ import SkillsSection from '../js/components/SkillsSection'
 import Jobs from '../js/components/Jobs'
 import Bio from '../js/components/Bio'
 import PageSection from '../js/components/PageSection'
-import { Box } from '@material-ui/core'
+import { Box, Typography, Container } from '@material-ui/core'
+import PageSubTitle from '../js/components/PageSubTitle'
 
 const Home = () => {
   return (
@@ -16,7 +17,21 @@ const Home = () => {
           <SkillsSection />
         </Box>
       </PageSection>
+
       <Jobs />
+
+      <PageSection>
+        <Container maxWidth="md">
+          <PageSubTitle text="Education" />
+          <Typography variant="h6">
+            Bachelor of Applied Science (B.A.Sc.), Computer science
+          </Typography>
+          <Typography color="textSecondary">
+            Novosibirsk State Technical University
+          </Typography>
+          <Typography color="textSecondary">2009-2013</Typography>
+        </Container>
+      </PageSection>
     </>
   )
 }
