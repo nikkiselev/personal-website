7d73b4a2da61d847eff91714c9d2569b7741e4e7
Nikolai Kiselev <nik@kiselev.dev>
2020-11-05 14:52:20 +0400

    Replace MUI avatar with just image

--- src/js/components/Avatar.tsx
+++ src/js/components/Avatar.tsx
@@ -3,8 +3,8 @@ import { Avatar as MUIAvatar, Box } from '@material-ui/core'
 const size = '280px!important'
 
 const Avatar = () => (
-  <Box height={size} width={size} clone>
-    <MUIAvatar src="/images/avatar.jpeg" alt="Nikolai Kiselev avatar" />
+  <Box height={size} width={size} borderRadius="50%" clone>
+    <img src="/images/avatar.jpeg" alt="Nikolai Kiselev avatar" />
   </Box>
 )
 
