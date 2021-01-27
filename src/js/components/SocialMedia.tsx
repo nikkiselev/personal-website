f47eb094f23a5b12cbf71661ae5b470fa33b5b87
Nikolai Kiselev <nik@kiselev.dev>
2021-01-27 18:56:06 +0800

    Refactor

--- src/js/components/SocialMedia.tsx
+++ src/js/components/SocialMedia.tsx
@@ -2,35 +2,39 @@ import GitHub from '@material-ui/icons/GitHub'
 import LinkedIn from '@material-ui/icons/LinkedIn'
 import StackOverflow from './StackOverflow'
 
-const SocialMedia = () => (
-  <>
-    <a
-      href="https://www.linkedin.com/in/nikkiselev"
-      aria-label="Nikolai Kiselev LinkedIn profile"
-    >
-      <LinkedIn htmlColor="#fff" fontSize="large" titleAccess="LinkedIn" />
-    </a>
-    &nbsp;
-    <a
-      href="https://stackoverflow.com/users/5964023/nikolai-kiselev?tab=profile"
-      aria-label="Nikolai Kiselev Stack Overflow profile"
-    >
-      <StackOverflow
-        htmlColor="#fff"
-        fontSize="large"
-        titleAccess="Stack Overflow"
-      />
-    </a>
-    &nbsp;
-    <a href="https://github.com/nikkiselev">
-      <GitHub
-        htmlColor="#fff"
-        fontSize="large"
-        titleAccess="GitHub"
-        aria-label="Nikolai Kiselev GitHub profile"
-      />
-    </a>
-  </>
-)
+const SocialMedia = () => {
+  const color = '#fff'
+
+  return (
+    <>
+      <a
+        href="https://www.linkedin.com/in/nikkiselev"
+        aria-label="Nikolai Kiselev LinkedIn profile"
+      >
+        <LinkedIn htmlColor={color} fontSize="large" titleAccess="LinkedIn" />
+      </a>
+      &nbsp;
+      <a
+        href="https://stackoverflow.com/users/5964023/nikolai-kiselev?tab=profile"
+        aria-label="Nikolai Kiselev Stack Overflow profile"
+      >
+        <StackOverflow
+          htmlColor={color}
+          fontSize="large"
+          titleAccess="Stack Overflow"
+        />
+      </a>
+      &nbsp;
+      <a href="https://github.com/nikkiselev">
+        <GitHub
+          htmlColor={color}
+          fontSize="large"
+          titleAccess="GitHub"
+          aria-label="Nikolai Kiselev GitHub profile"
+        />
+      </a>
+    </>
+  )
+}
 
 export default SocialMedia
