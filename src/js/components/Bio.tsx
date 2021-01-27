8315ced60c92eb482511c2a4640b67b14bed4030
Nikolai Kiselev <nik@kiselev.dev>
2021-01-27 18:21:29 +0800

    Remove React logo

--- src/js/components/Bio.tsx
+++ src/js/components/Bio.tsx
@@ -1,6 +1,5 @@
 import { Container, Grid, Box, Typography, Fade } from '@material-ui/core'
 import PageSection from './PageSection'
-import ReactLogo from './ReactLogo'
 import { useState, useEffect } from 'react'
 import config from 'js/config'
 import content from 'content/home.json'
@@ -17,21 +16,6 @@ const Bio = () => {
       <Fade in={showContent} timeout={config.timeoutContentFade}>
         <Container maxWidth="md">
           <Grid container>
-            <Box clone pb={{ xs: 3, md: 0 }}>
-              <Grid item xs={12} md={6}>
-                <Box
-                  display="flex"
-                  flexDirection="column"
-                  justifyContent="center"
-                  alignItems={{ xs: 'center', md: 'initial' }}
-                  height="100%!important"
-                >
-                  <Box width="60%!important" height="auto!important" clone>
-                    <ReactLogo />
-                  </Box>
-                </Box>
-              </Grid>
-            </Box>
             <Grid item xs={12} md={6}>
               <Box textAlign={{ xs: 'center', md: 'left' }}>
                 {content.bio.map((text) => (
