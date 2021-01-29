d83080441fa44ebd2454a560e9e1b03c76661bdb
Nikolai Kiselev <nik@kiselev.dev>
2021-01-29 19:01:09 +0800

    Add dividers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,19 @@ import SkillsSection from 'js/components/SkillsSection'
 import Bio from 'js/components/Bio'
 import Jobs from 'js/components/Jobs'
 import Education from 'js/components/Education'
+import { Divider } from '@material-ui/core'
 
 const Home = () => {
   return (
     <>
       <SubHeader />
+      <Divider variant="middle" />
       <Bio />
+      <Divider variant="middle" />
       <SkillsSection />
+      <Divider variant="middle" />
       <Jobs />
+      <Divider variant="middle" />
       <Education />
     </>
   )
