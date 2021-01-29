c3c30cf374ae27e96da5d6e01a76d58c10fb3111
Nikolai Kiselev <nik@kiselev.dev>
2021-01-29 23:29:46 +0800

    Remove button

--- src/js/components/SubHeaderActions.tsx
+++ src/js/components/SubHeaderActions.tsx
@@ -4,16 +4,8 @@ const SubHeaderActions = () => (
   <Grid container spacing={2}>
     <Grid item xs={12} sm={6}>
       <Box clone width="100%">
-        <Button variant="contained" color="primary" href="#work-with-me">
+        <Button variant="outlined" color="primary" href="#work-with-me">
           Work with me
-        </Button>
-      </Box>
-    </Grid>
-
-    <Grid item xs={12} sm={6}>
-      <Box clone width="100%">
-        <Button variant="contained" color="secondary" href="#skills">
-          View Skills
         </Button>
       </Box>
     </Grid>
