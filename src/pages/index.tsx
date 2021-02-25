6e37af0a63eee3e133842424bb600ae92164ad25
Nikolai Kiselev <nik@kiselev.dev>
2021-02-25 10:40:49 +0800

    Migrate index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,30 +3,22 @@ import SkillsSection from 'js/components/SkillsSection'
 import Bio from 'js/components/Bio'
 import Jobs from 'js/components/Jobs'
 import Education from 'js/components/Education'
-import { Grid } from '@material-ui/core'
-import { Container } from 'next/app'
 import ContentDivider from 'js/components/ContentDivider'
 import PageSubTitle from 'js/components/PageSubTitle'
 
 const Home = () => {
   return (
-    <>
-      <Container maxWidth="md">
-        <Grid container justify="center">
-          <Grid item>
-            <SubHeader />
-            <ContentDivider />
-            <Bio />
-            <PageSubTitle text="Skill Set" id="skills" />
-            <SkillsSection />
-            <PageSubTitle text="Experience" id="experience" />
-            <Jobs />
-            <PageSubTitle text="Education" />
-            <Education />
-          </Grid>
-        </Grid>
-      </Container>
-    </>
+    <div className="container mx-auto">
+      <SubHeader />
+      <ContentDivider />
+      <Bio />
+      <PageSubTitle text="Skill Set" id="skills" />
+      <SkillsSection />
+      <PageSubTitle text="Experience" id="experience" />
+      <Jobs />
+      <PageSubTitle text="Education" />
+      <Education />
+    </div>
   )
 }
 
