39c4759c7390d4812a3bd68bd5217b6ca8354523
Nikolai Kiselev <nik@kiselev.dev>
2021-02-25 10:34:58 +0800

    Migrate Avatar

--- src/js/components/Avatar.tsx
+++ src/js/components/Avatar.tsx
@@ -1,18 +1,16 @@
-import { Box } from '@material-ui/core'
 import Image from 'next/image'
 
 const size = '280px!important'
 
 const Avatar = () => (
-  <Box borderRadius="50%" clone>
-    <Image
-      src="/images/avatar.jpeg"
-      height={size}
-      width={size}
-      loading="eager"
-      alt="Nikolai Kiselev avatar"
-    />
-  </Box>
+  <Image
+    className="rounded-full"
+    src="/images/avatar.jpeg"
+    height={size}
+    width={size}
+    loading="eager"
+    alt="Nikolai Kiselev avatar"
+  />
 )
 
 export default Avatar
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   purge: [],
   //@todo test purge
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/js/components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/js/components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
