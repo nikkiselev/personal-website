d594628d03b19bac83963480ccc9d1418839e669
Nikolai Kiselev <nik@kiselev.dev>
2021-05-02 14:35:54 +0800

    Fix padding on mobile

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en" prefix="og: http://ogp.me/ns#">
         <Head></Head>
-        <body className="py-10 mx-auto prose">
+        <body className="py-10 mx-auto prose container">
           <Main />
           <NextScript />
         </body>
