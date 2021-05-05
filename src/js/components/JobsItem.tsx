506b85a0261ee0902cd91012db3f88d83014cf4d
Nikolai Kiselev <nik@kiselev.dev>
2021-05-05 15:51:25 +0800

    Fix styles

--- src/js/components/JobsItem.tsx
+++ src/js/components/JobsItem.tsx
@@ -10,14 +10,11 @@ const JobsItem = (props: Props) => {
   return (
     <li>
       <article>
-        <div className="grid md:grid-cols-8 xl:grid-cols-9 items-start relative rounded-xl p-3 sm:p-5 xl:p-6 overflow-hidden hover:bg-white">
-          <h3 className="text-gray-900 md:col-start-3 md:col-span-6 xl:col-start-3 xl:col-span-7 mb-1 ml-9 md:ml-0">
-            {props.job.role}
-          </h3>
-          {/* <time dateTime="2021-04-05T19:00:00.000Z" className="md:col-start-1 md:col-span-2 row-start-1 md:row-end-3 flex items-center font-medium mb-1 md:mb-0"><svg viewBox="0 0 12 12" className="w-3 h-3 mr-6 overflow-visible text-cyan-400"><circle cx="6" cy="6" r="6" fill="currentColor"></circle><circle cx="6" cy="6" r="11" fill="none" stroke="currentColor" stroke-width="2"></circle><path d="M 6 18 V 500" fill="none" stroke-width="2" stroke="currentColor" className="text-gray-200"></path></svg>Apr 6, 2021</time> */}
+        <div className="grid items-start relative rounded-xl p-3 sm:p-5 xl:p-6 overflow-hidden hover:bg-white">
+          <h3 className="text-gray-900  mb-1 ml-9 ">{props.job.role}</h3>
           <time
             dateTime="2021-03-15T16:30:00.000Z"
-            className="md:col-start-1 md:col-span-2 row-start-1 md:row-end-3 flex items-center font-medium mb-1 md:mb-0"
+            className="row-start-1 flex items-center font-medium mb-1 "
           >
             <svg
               viewBox="0 0 12 12"
@@ -41,7 +38,7 @@ const JobsItem = (props: Props) => {
             </svg>
             {duration}
           </time>
-          <div className="md:col-start-3 md:col-span-6 xl:col-span-7 ml-9 md:ml-0">
+          <div className="ml-9">
             <span>
               {props.job.company}&nbsp;&nbsp;&middot;&nbsp;&nbsp;
               {props.job.type}
