fafd044307b62b344101f22445c366e3f7049893
Nikolai Kiselev <nik@kiselev.dev>
2021-05-18 17:06:52 +0800

    Refactor

--- src/js/components/ThemeToggle.tsx
+++ src/js/components/ThemeToggle.tsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from 'react'
 import { Switch } from '@headlessui/react'
 
+//@todo refactor
+const applyDarkTheme = (): void =>
+  document.documentElement.classList.add('dark')
+const applyLightTheme = (): void =>
+  document.documentElement.classList.remove('dark')
+const saveTheme = (theme: string): string => (localStorage.theme = theme)
+
 export default function ThemeToggle() {
   const [enabled, setEnabled] = useState(false)
 
-  const toggle = (v: boolean) => {
+  const setDarkTheme = (v: boolean) => {
     setEnabled(v)
+
     if (v) {
-      localStorage.theme = 'dark'
-      document.documentElement.classList.add('dark')
+      saveTheme('dark')
+      applyDarkTheme()
     } else {
-      localStorage.theme = 'light'
-      document.documentElement.classList.remove('dark')
+      saveTheme('light')
+      applyLightTheme()
     }
   }
 
@@ -20,13 +28,13 @@ export default function ThemeToggle() {
       return
     }
 
-    toggle(localStorage.theme === 'dark')
+    setDarkTheme(localStorage.theme === 'dark')
   }, [])
 
   return (
     <Switch
       checked={enabled}
-      onChange={toggle}
+      onChange={setDarkTheme}
       style={{ width: 74, height: 38 }}
       className={`${enabled ? 'bg-green-900' : 'bg-green-700'}
           relative inline-flex flex-shrink-0 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75 transform scale-50`}
