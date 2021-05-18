70b09342087cb30ad032a4aa9427fc4406ed2c5d
Nikolai Kiselev <nik@kiselev.dev>
2021-05-18 16:16:50 +0800

    Add theme toggling

--- src/js/components/ThemeToggle.tsx
+++ src/js/components/ThemeToggle.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Switch } from '@headlessui/react'
 
 export default function ThemeToggle() {
   const [enabled, setEnabled] = useState(false)
 
+  const toggle = (v: boolean) => {
+    setEnabled(v)
+  }
+
   return (
     <Switch
       checked={enabled}
-      onChange={setEnabled}
+      onChange={toggle}
       style={{ width: 74, height: 38 }}
       className={`${enabled ? 'bg-green-900' : 'bg-green-700'}
           relative inline-flex flex-shrink-0 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75 transform scale-50`}
