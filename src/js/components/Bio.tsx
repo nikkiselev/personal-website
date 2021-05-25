9859e93742c27482aafe67e465651c5f2dcdbd2b
Nikolai Kiselev <nik@kiselev.dev>
2021-05-25 17:16:43 +0800

    Add Ps

--- src/js/components/Bio.tsx
+++ src/js/components/Bio.tsx
@@ -4,7 +4,20 @@ const Bio = () => {
   return (
     <div>
       <h2>{content.bioTitle}</h2>
-      <p>{content.bio}</p>
+      <p>
+        I still remember my first encounter with programming in elementary
+        school at the age of 8. At 12, I was enrolled in a programming course. I
+        wrote small scripts and apps using Pascal.
+      </p>
+      <p>
+        In 2004, I built my first website about the history of Earth. It wasn't
+        great but it made me realize my passion for programming. That led me
+        toward getting a Bachelor degree in Computer Science.{' '}
+      </p>
+      <p>
+        Fast forward to today, I build web applications as a freelance
+        full-stack web developer.
+      </p>
     </div>
   )
 }
