1d3381b22280c19cf1cdddcd52c95bd70b05c1aa
Nikolai Kiselev <nik@kiselev.dev>
2021-05-25 14:06:37 +0800

    Fix alignment

--- src/js/components/ThemeToggle.tsx
+++ src/js/components/ThemeToggle.tsx
@@ -18,20 +18,22 @@ export default function ThemeToggle() {
   }, [])
 
   return (
-    <Switch
-      checked={enabled}
-      onChange={setDarkTheme}
-      style={{ width: 74, height: 38 }}
-      className={`${enabled ? 'bg-green-900' : 'bg-green-700'}
-          relative inline-flex flex-shrink-0 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75 transform scale-50`}
-    >
-      <span className="sr-only">Use setting</span>
-      <span
-        aria-hidden="true"
-        style={{ width: 34, height: 34 }}
-        className={`${enabled ? 'translate-x-9' : 'translate-x-0'}
+    <div className="transform scale-50 origin-right">
+      <Switch
+        checked={enabled}
+        onChange={setDarkTheme}
+        style={{ width: 74, height: 38 }}
+        className={`${enabled ? 'bg-green-900' : 'bg-green-700'}
+          relative inline-flex flex-shrink-0 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+      >
+        <span className="sr-only">Use setting</span>
+        <span
+          aria-hidden="true"
+          style={{ width: 34, height: 34 }}
+          className={`${enabled ? 'translate-x-9' : 'translate-x-0'}
             pointer-events-none inline-block rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
-      />
-    </Switch>
+        />
+      </Switch>
+    </div>
   )
 }
